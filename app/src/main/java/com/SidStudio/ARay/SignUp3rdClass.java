1580d7c0faec06968b7f3ad9f8b752f9c350fa90
Siddhesh Pansare <61089801+Siddhesh-hub@users.noreply.github.com>
2021-02-24 19:16:02 +0530

    Forget pwd

--- app/src/main/java/com/SidStudio/ARay/SignUp3rdClass.java
+++ app/src/main/java/com/SidStudio/ARay/SignUp3rdClass.java
@@ -2,13 +2,36 @@ package com.SidStudio.ARay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.widget.ScrollView;
 
 public class SignUp3rdClass extends AppCompatActivity {
+
+    ScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_sign_up3rd_class);
+
+        scrollView = findViewById(R.id.signup_3rd_screen_scroll_view);
+    }
+
+    public void callOTPVerification(View view) {
+        Intent intent = new Intent(getApplicationContext(), VerifyOTP.class);
+
+//Add Transition
+        Pair[] pairs = new Pair[1];
+        pairs[0] = new Pair<View, String>(scrollView, "transition_OTP_screen");
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SignUp3rdClass.this, pairs);
+            startActivity(intent, options.toBundle());
+        } else {
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/SidStudio/ARay/VerifyOTP.java
@@ -0,0 +1,15 @@
+package com.SidStudio.ARay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VerifyOTP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_verify_o_t_p);
+    }
+
+}
